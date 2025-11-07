fbf2aab479468606bfe2b988352e4d89a2f7efc6
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2025-11-07 22:12:58 +0000

    Create functions to create updated tag according to specific parameters

---
+++ src/utils/update_tags.ts
@@ -0,0 +1,117 @@
+import { SearchType, VersionTag } from './types.js'
+import { fullTagFromObject, tagNameFromNumber, versionRegex } from './utils.js'
+
+/**
+ * @example
+ * updatePrerelease({ fullTag: 'v1.2.3-beta.2' })
+ * @returns
+ * 'v1.2.3-beta.3'
+ */
+export function updatePrerelease(tag: VersionTag): VersionTag {
+  const updated_tag: VersionTag = {
+    fullTag: '',
+    prefix: tag.prefix,
+    tagName: '',
+    suffix: tag.suffix,
+    number: { major: 0, minor: 0, patch: 0 }
+  }
+
+  if (
+    versionRegex(SearchType.NO_SUFFIX).test(tag.fullTag) ||
+    versionRegex(SearchType.WITH_SUFFIX).test(tag.fullTag)
+  ) {
+    updated_tag.number = {
+      major: tag.number.major,
+      minor: tag.number.minor + 1,
+      patch: 0,
+      prerelease: 1
+    }
+    updated_tag.prerelease_number = '1'
+    updated_tag.tagName = tagNameFromNumber(updated_tag.number)
+    updated_tag.fullTag = fullTagFromObject(updated_tag)
+  } else {
+    updated_tag.number = {
+      ...tag.number,
+      prerelease: tag.number.prerelease ? tag.number.prerelease + 1 : 1
+    }
+    updated_tag.prerelease_number = updated_tag.number.prerelease?.toString()
+    updated_tag.tagName = tagNameFromNumber(updated_tag.number)
+    updated_tag.fullTag = fullTagFromObject(updated_tag)
+  }
+
+  return updated_tag
+}
+
+/**
+ * @example
+ * updatePatch({ fullTag: 'v1.2.3' })
+ * @returns
+ * 'v1.2.4'
+ */
+export function updatePatch(tag: VersionTag): VersionTag {
+  const updated_tag: VersionTag = {
+    fullTag: '',
+    prefix: tag.prefix,
+    tagName: '',
+    suffix: tag.suffix,
+    number: { ...tag.number, patch: tag.number.patch + 1 }
+  }
+
+  updated_tag.tagName = tagNameFromNumber(updated_tag.number)
+  updated_tag.fullTag = fullTagFromObject(updated_tag)
+
+  return updated_tag
+}
+
+/**
+ * @example
+ * updateMinor({ fullTag: 'v1.2.3' })
+ * @returns
+ * 'v1.3.0'
+ */
+export function updateMinor(tag: VersionTag): VersionTag {
+  const updated_tag: VersionTag = {
+    fullTag: '',
+    prefix: tag.prefix,
+    tagName: '',
+    suffix: tag.suffix,
+    number: { major: tag.number.major, minor: tag.number.minor + 1, patch: 0 }
+  }
+
+  updated_tag.tagName = tagNameFromNumber(updated_tag.number)
+  updated_tag.fullTag = fullTagFromObject(updated_tag)
+
+  return updated_tag
+}
+
+// TODO Create documentation form examples
+/**
+ * @param {VersionTag} tag - The latest relevant tag found
+ * @example
+ * updateMajor({ fullTag: 'v1.2.3' })
+ * @returns
+ * 'v2.0.0'
+ */
+export function updateMajor(tag: VersionTag): VersionTag {
+  const updated_tag: VersionTag = {
+    fullTag: '',
+    prefix: tag.prefix,
+    tagName: '',
+    suffix: tag.suffix,
+    number: { major: tag.number.major + 1, minor: 0, patch: 0 }
+  }
+
+  updated_tag.tagName = tagNameFromNumber(updated_tag.number)
+  updated_tag.fullTag = fullTagFromObject(updated_tag)
+
+  return updated_tag
+}
+
+export function updateNone(tag: VersionTag): VersionTag {
+  const updated_tag: VersionTag = tag
+
+  updated_tag.tagName = tagNameFromNumber(updated_tag.number)
+  updated_tag.fullTag = fullTagFromObject(updated_tag)
+
+  return updated_tag
+}
