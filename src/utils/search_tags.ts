d7b304985df5650f66d7122df0007d7c17cce018
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2025-11-10 06:24:17 +0000

    Update search_tags.ts

--- src/utils/search_tags.ts
+++ src/utils/search_tags.ts
@@ -1,76 +1,78 @@
-import { SearchType, VersionList, VersionTag } from './types.js'
+import { getPrefix, getSuffix } from './utils.js'
+import { SearchType, VersionTag } from './types.js'
 import { sortVersions, versionRegex } from './utils.js'
 
-export function searchPrerelease(tagList: VersionList): VersionTag {
-  const match_result: VersionList = {
-    prefix: tagList.prefix,
-    tags: [],
-    suffix: tagList.suffix
-  }
+/**
+ * Returns the latest prerelease tag
+ * @param {VersionTag[]} tagList - The list of tags in the repository
+ * @example searchPrerelease([{ fullTag: 'v1.2.3' }, { fullTag: 'v2.3.4-beta.3' }])
+ * @returns {VersionTag} { fullTag: 'v2.3.4-beta.3' }
+ */
+export function searchPrerelease(tagList: VersionTag[]): VersionTag {
+  let match_list: VersionTag[] = []
 
-  match_result.tags = tagList.tags.filter((tag) =>
-    tag.fullTag.match(versionRegex(SearchType.PRERELEASE))
+  match_list = tagList.filter((tag) =>
+    versionRegex(SearchType.PRERELEASE).test(tag.fullTag)
   )
 
-  if (match_result.tags.length === 0) {
-    match_result.tags = tagList.tags.filter((tag) =>
-      tag.fullTag.match(versionRegex(SearchType.WITH_SUFFIX))
+  if (match_list.length === 0) {
+    match_list = tagList.filter((tag) =>
+      versionRegex(SearchType.WITH_SUFFIX).test(tag.fullTag)
     )
   }
 
-  if (match_result.tags.length === 0) {
-    match_result.tags = tagList.tags.filter((tag) =>
-      tag.fullTag.match(versionRegex(SearchType.NO_SUFFIX))
+  if (match_list.length === 0) {
+    match_list = tagList.filter((tag) =>
+      versionRegex(SearchType.NO_SUFFIX).test(tag.fullTag)
     )
   }
 
-  const matched_tags: VersionList = match_result.tags
-    ? sortVersions(match_result)
-    : { prefix: tagList.prefix, tags: [], suffix: tagList.suffix }
+  const matched_tags: VersionTag[] = match_list ? sortVersions(match_list) : []
 
-  const tag: VersionTag | undefined = matched_tags.tags.pop()
+  const tag: VersionTag | undefined = matched_tags.pop()
 
   const latest_tag: VersionTag = {
-    fullTag: tag ? tag.fullTag : `${tagList.prefix}0.0.0`,
-    prefix: tagList.prefix,
+    fullTag: tag ? tag.fullTag : `${getPrefix()}0.0.0`,
+    prefix: getPrefix(),
     tagName: tag ? tag.tagName : '0.0.0',
-    suffix: tagList.suffix,
+    suffix: getSuffix(),
     prerelease_number: tag?.prerelease_number,
     number: tag?.number ? tag.number : { major: 0, minor: 0, patch: 0 }
   }
+
   return latest_tag
 }
 
-export function searchBase(tagList: VersionList): VersionTag {
-  const match_result: VersionList = {
-    prefix: tagList.prefix,
-    tags: [],
-    suffix: tagList.suffix
-  }
+/**
+ * Returns the latest version without prerelease component and the provided suffix, if any
+ * @param {VersionTag[]} tagList - The list of tags in the repository
+ * @example searchBase([{ fullTag: 'v1.2.3' }, { fullTag: 'v2.3.4-beta' }])
+ * @returns {VersionTag} { fullTag: 'v2.3.4-beta' }
+ */
+export function searchBase(tagList: VersionTag[]): VersionTag {
+  let match_list: VersionTag[] = []
 
-  if (tagList.suffix !== '') {
-    match_result.tags = tagList.tags.filter((tag) =>
+  if (getSuffix() !== '') {
+    match_list = tagList.filter((tag) =>
       tag.fullTag.match(versionRegex(SearchType.WITH_SUFFIX))
     )
   }
 
-  if (match_result.tags.length === 0) {
-    match_result.tags = tagList.tags.filter((tag) =>
+  if (match_list.length === 0) {
+    match_list = tagList.filter((tag) =>
       tag.fullTag.match(versionRegex(SearchType.NO_SUFFIX))
     )
   }
 
-  const matched_tags: VersionList = match_result.tags
-    ? sortVersions(match_result)
-    : { prefix: tagList.prefix, tags: [], suffix: tagList.suffix }
+  const matched_tags: VersionTag[] = match_list ? sortVersions(match_list) : []
 
-  const tag: VersionTag | undefined = matched_tags.tags.pop()
+  const tag: VersionTag | undefined = matched_tags.pop()
 
   const latest_tag: VersionTag = {
-    fullTag: tag ? tag.fullTag : `${tagList.prefix}0.0.0`,
-    prefix: tagList.prefix,
+    fullTag: tag ? tag.fullTag : `${getPrefix()}0.0.0`,
+    prefix: getPrefix(),
     tagName: tag ? tag.tagName : '0.0.0',
-    suffix: tagList.suffix,
+    suffix: getSuffix(),
     number: tag?.number ? tag.number : { major: 0, minor: 0, patch: 0 }
   }
 
