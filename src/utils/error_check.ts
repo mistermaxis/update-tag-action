9893657b5700f337a58bae8ab085d19039b8351a
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2025-12-14 22:54:51 +0000

    Update input names and variants in error functions

--- src/utils/error_check.ts
+++ src/utils/error_check.ts
@@ -1,25 +1,41 @@
 import * as core from '@actions/core'
-import { getBump, getCopyFrom, getSuffix } from './utils.js'
+import { getBump, getNewSuffix, getReplaceSuffix, getSuffix } from './utils.js'
 import { BumpType } from './types.js'
 
 export function checkForErrors() {
   const bump = getBump()
   const suffix = getSuffix()
-  const copy_from = getCopyFrom()
+  const replace_suffix = getReplaceSuffix()
 
-  if (bump === BumpType.PRERELEASE && !suffix) {
+  if (
+    (bump === BumpType.PREMAJOR ||
+      bump === BumpType.PREMINOR ||
+      bump === BumpType.PREPATCH) &&
+    !suffix
+  ) {
     throw new Error('Prerelease bumps must be used with a suffix')
   }
 
-  if (bump === BumpType.PRERELEASE && copy_from === true) {
+  if (
+    (bump === BumpType.PREMAJOR ||
+      bump === BumpType.PREMINOR ||
+      bump === BumpType.PREPATCH) &&
+    !suffix
+  ) {
     throw new Error(
-      'The flag copy_from:true is not meant to be used with bump:prerelease'
+      'The flag replace_suffix:true is not meant to be used with prerelease bumps'
     )
   }
 
-  if (suffix && copy_from == true) {
+  if (replace_suffix === true && getNewSuffix() === undefined) {
+    throw new Error(
+      'A new_suffix must be defined when using replace_suffix:true'
+    )
+  }
+
+  if (suffix && replace_suffix === true) {
     core.warning(
-      'If there is no tag with the provided suffix, copy_from is unnecessary'
+      'If there is no tag with the provided suffix, replace_suffix is unnecessary'
     )
   }
 }
