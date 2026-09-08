e9abf4cff5e9ef7760a2bda9c9119196b5cc29c8
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2026-09-08 11:41:59 +0000

    Try different format for changelog output

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -53,7 +53,7 @@ export async function listCommits(): Promise<Commit[]> {
       pull_number: pull_request?.number
     })
     const changelog = response.data.map(
-      (data) => `- [${data.commit.message}](${data.commit.tree.url})`
+      (data) => `- [${data.commit.message}](${data.html_url})`
     )
     core.setOutput('changelog', changelog ? changelog.join('\n') : '')
   }
