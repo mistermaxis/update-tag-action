1162f14fa6ed3bec031e9d92b012e10fa715d106
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2026-09-08 08:44:16 +0000

    Add listCommits test function

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
 import { context, getOctokit } from '@actions/github'
-import { SearchType, VersionTag } from './types.js'
+import { Commit, SearchType, VersionTag } from './types.js'
 import { stripVersionNumber, tagToNumber, versionRegex } from './utils.js'
 import { getPrefix, getSuffix } from './utils.js'
 
@@ -30,6 +30,15 @@ export async function listTags(): Promise<VersionTag[]> {
       number: tagToNumber(tag.name)
     }
   })
-
+  listCommits()
   return tags
+}
+
+export async function listCommits(): Promise<Commit[]> {
+  //const githubToken = core.getInput('github_token')
+  //const octokit = getOctokit(githubToken)
+  //const { owner, repo } = context.repo
+  console.log(context.payload.commits)
+
+  return []
 }
