22d8abd6c1baa8c73696078887d4ca586d8831da
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2026-09-09 06:20:24 +0000

    Rebuild dist files

--- dist/index.js
+++ dist/index.js
@@ -33988,6 +33988,50 @@ async function listTags() {
     });
     return tags;
 }
+async function outputCommits() {
+    if (context.eventName === 'push') {
+        const payload = context.payload;
+        const commits = payload.commits?.map((commit) => {
+            return `- ${commit.message} - ${commit.id}`;
+        });
+        const commitArray = payload.commits?.map((commit) => ({
+            id: commit.id,
+            message: commit.message,
+            url: commit.url,
+            timestamp: commit.timestamp,
+            author: {
+                name: commit.author.name
+            }
+        }));
+        setOutput('commits', commitArray ? JSON.stringify(commitArray) : '');
+        setOutput('changelog', commits ? commits.join('\n') : '');
+    }
+    else if (context.eventName === 'pull_request') {
+        const githubToken = getInput('github_token');
+        const octokit = getOctokit(githubToken);
+        const { owner, repo } = context.repo;
+        const pull_request = context.payload.pull_request;
+        const response = await octokit.rest.pulls.listCommits({
+            owner,
+            repo,
+            per_page: 250,
+            page: 1,
+            pull_number: pull_request?.number
+        });
+        const changelog = response.data.map((data) => `- ${data.commit.message} - ${data.sha}`);
+        const commitArray = response.data.map((data) => ({
+            id: data.sha,
+            message: data.commit.message,
+            url: data.html_url,
+            timestamp: data.commit.author?.date ?? '',
+            author: {
+                name: data.commit.author?.name ?? ''
+            }
+        }));
+        setOutput('commits', commitArray ? JSON.stringify(commitArray) : '');
+        setOutput('changelog', changelog ? changelog.join('\n') : '');
+    }
+}
 
 /**
  * Returns the latest prerelease tag
@@ -34200,6 +34244,7 @@ async function run() {
                 break;
         }
         // Set outputs for other workflow steps to use
+        outputCommits();
         setOutput('updated_tag', updated_tag.fullTag);
     }
     catch (error) {
