b9d53b003dc66e82bd8b3d30059f573c2b0cf069
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2026-09-14 01:28:36 +0000

    Add outputCommits mock function to tests

--- __fixtures__/github.ts
+++ __fixtures__/github.ts
@@ -2,3 +2,5 @@ import { jest } from '@jest/globals'
 
 export const listTags =
   jest.fn<typeof import('../src/utils/github.js').listTags>()
+export const outputCommits =
+  jest.fn<typeof import('../src/utils/github.js').outputCommits>()
